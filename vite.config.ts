9ad39ef1deac0ff59938ac3650c3666a6a9d50fb
Noelle Stern <noellesternchen@gmail.com>
2025-09-26 14:12:18 +0500

    PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import Icons from 'unplugin-icons/vite';
@@ -15,6 +16,51 @@ export default defineConfig({
     Icons({
       defaultStyle: 'width: calc(var(--spacing) * 10); height: calc(var(--spacing) * 10);',
       compiler: 'svelte',
+    }),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest,json}"],
+      },
+      // generates 'manifest.webmanifest' file on build
+      manifest: {
+        name: 'Save Editor Nine Sols Edition',
+        short_name: 'SENSE',
+        start_url: '/Save-Editor-Nine-Sols-Edition/',
+        display: 'standalone',
+        description: 'A web-based Nine Sols save file editor.',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'images/icons/pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'images/icons/pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+        ],
+        "screenshots": [
+          // Desktop
+          {
+            "src": "images/screenshots/wide1.png",
+            "sizes": "1920x1080",
+            "type": "image/png",
+            "form_factor": "wide",
+            "label": "Welcome screen"
+          },
+          {
+            "src": "images/screenshots/wide2.png",
+            "sizes": "1920x1080",
+            "type": "image/png",
+            "form_factor": "wide",
+            "label": "Editor screen"
+          },
+        ],
+      }
     }),
   ],
   optimizeDeps: {
